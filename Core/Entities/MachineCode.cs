3b01854e8277fd68b5064d8e3e94f70bb23f7c71
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-02 07:36:50 -0600

    feat: entidades

---
+++ Core/Entities/MachineCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class MachineCode
+    {
+        public int Id { get; set; }
+
+        public string MachineCodeName { get; set; } = string.Empty;
+
+        public int ProcessId { get; set; }
+
+        public Process Process { get; set; } = null!;
+    }
+}
---
+++ Core/Entities/TypeScrap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class TypeScrap
+    {
+        public int Id { get; set; }
+
+        public string TypeScrapName { get; set; }
+
+        public ICollection<Defect> Defects { get; set; } = new List<Defect>();
+    }
+}

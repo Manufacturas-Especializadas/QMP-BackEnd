80a6894bbe51a0b508b6f64ab7bd350a49f1b0ec
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-03 09:22:54 -0600

    feat: metodos para registrar y guardar un scrap

---
+++ Infrastructure/Repositories/ScrapRepository.cs
@@ -0,0 +1,33 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ScrapRepository : IScrapRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ScrapRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Scrap> CreateAsync(Scrap scrap)
+        {
+            await _context.Scraps.AddAsync(scrap);
+
+            return scrap;
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}

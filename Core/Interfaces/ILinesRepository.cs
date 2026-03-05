acfe7f4b273b73009ded66314bca277483064d2a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-05 15:39:25 -0600

    feat: metodo para el registro de lineas

---
+++ Core/Interfaces/ILinesRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface ILinesRepository
+    {
+        Task<Line> CreateAsync(Line line);
+
+        Task<bool> SaveChangesAsync();
+    }
+}

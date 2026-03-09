59323028bb425927bb5bfd11844edaee425a2e88
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-09 11:08:02 -0600

    feat: entidades role y usuario

--- Core/Entities/Role.cs
+++ Core/Entities/Role.cs
@@ -10,7 +10,7 @@ namespace Core.Entities
     {
         public int Id { get; set; }
 
-        public string Name { get; set; } = string.Empty;
+        public string RoleName { get; set; } = string.Empty;
 
         public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
     }
--- Core/Entities/User.cs
+++ Core/Entities/User.cs
@@ -12,17 +12,14 @@ namespace Core.Entities
 
         public string Username { get; set; } = string.Empty;
 
-        public string Email { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } = null!;
 
-        public byte[] PasswordHash { get; set; } = new byte[0];
-
-        public byte[] PasswordSalt { get; set; } = new byte[0];
+        public byte[] PasswordSalt { get; set; } = null!;
 
         public DateTime CreatedAt { get; set; } = DateTime.Now;
 
         public bool IsActive { get; set; } = true;
 
         public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
-
     }
 }

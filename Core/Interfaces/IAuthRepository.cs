4dfef716e99f6a229b0792afbff4a8c48ded9cb0
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-09 11:05:24 -0600

    feat: IRepository Auth

---
+++ Core/Interfaces/IAuthRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+
+        Task<User> Login(string employeeNumber, string password);
+
+        Task<bool> UserExists(string employeeNumber);
+    }
+}

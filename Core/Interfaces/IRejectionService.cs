349368e5d8c84dbe7f73c8216baaa338fbc8af91
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-30 07:15:06 -0600

    feat: interfaz de rechazos

---
+++ Core/Interfaces/IRejectionService.cs
@@ -0,0 +1,16 @@
+﻿using Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IRejectionService
+    {
+        Task<int> CreateRejectionAsync(CreateRejectionDto dto, int userId);
+
+        Task UpdateRejectionAsync(EditRejectionDto dto);
+    }
+}

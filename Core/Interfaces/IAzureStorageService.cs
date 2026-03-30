3fa600da74e613c60ccc3d2199bd76b4e1cfbdae
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-30 07:14:51 -0600

    feat: interfaz azureblobstorage

---
+++ Core/Interfaces/IAzureStorageService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IAzureStorageService
+    {
+        Task<string> UploadFileAsync(string container, IFormFile file);
+
+        Task<bool> DeleteFileAsync(string container, string fileUrl);
+
+        Task<Stream> DownloadFileAsync(string container, string fileName);
+    }
+}

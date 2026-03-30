5e49728cce5e6c37a84086a8f150aaff95a02507
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-30 07:12:37 -0600

    feat: entidad condiciones

---
+++ Core/Entities/Condition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Condition
+    {
+        public int Id { get; set; }
+
+        public int DefectId { get; set; }
+
+        public string ConditionName { get; set; }
+
+        public virtual DefectRejection DefectRejection { get; set; }
+    }
+}

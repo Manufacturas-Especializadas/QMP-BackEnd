950a9c15a5de8d536090fdb42df8eecf3c2e2498
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-30 07:15:31 -0600

    feat: interfaz repositorio de rechazos

---
+++ Core/Interfaces/IRejectionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IRejectionRepository
+    {
+        Task<Rejection> GetByIdAsync(int id);
+
+        Task<int> CreateAsync(Rejection rejection);
+
+        Task UpdateAsync(Rejection rejection);
+
+        Task<bool> UserExistsAsync(int userId);
+    }
+}

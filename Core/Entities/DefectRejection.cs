ef9ddec38a3ad3919237bc1e55e73ac794aa6b95
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-30 07:14:31 -0600

    feat: entidad defecto

---
+++ Core/Entities/DefectRejection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class DefectRejection
+    {
+        public int Id { get; set; }
+
+        public string DefectName { get; set; } = string.Empty;
+
+        public virtual ICollection<Condition> Conditions { get; set; } = new List<Condition>();
+    }
+}

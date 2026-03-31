47bc452f40f5f39948d43afeadb0a04ba23ca868
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-31 09:41:29 -0600

    fix: nuevo mapeo del dbcontext

--- Infrastructure/Data/ApplicationDbContext.cs
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,5 @@
 ﻿using Core.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Data
 {
@@ -14,90 +9,72 @@ namespace Infrastructure.Data
         {
         }
 
-        public DbSet<Line> Lines => Set<Line>();
-
-        public DbSet<Client> Clients => Set<Client>();
-
-        public DbSet<Material> Materials => Set<Material>();
-
-        public DbSet<TypeScrap> TypeScraps => Set<TypeScrap>();
-
-        public DbSet<Shift> Shifts => Set<Shift>();
-
-        public DbSet<Defect> Defects => Set<Defect>();
-
-        public DbSet<Process> Processes => Set<Process>();
-
-        public DbSet<MachineCode> MachineCodes => Set<MachineCode>();
-
-        public DbSet<Scrap> Scraps => Set<Scrap>();
-
         public DbSet<User> Users => Set<User>();
+        public DbSet<Role> Roles => Set<Role>();
+        public DbSet<UserRole> UserRoles => Set<UserRole>();
 
         public DbSet<Rejection> Rejections => Set<Rejection>();
 
         public DbSet<DefectRejection> DefectsRejections => Set<DefectRejection>();
-
+        public DbSet<Condition> Conditions => Set<Condition>();
         public DbSet<ContainmentAction> ContainmentActions => Set<ContainmentAction>();
 
-        public DbSet<Condition> Conditions => Set<Condition>();
+        public DbSet<Line> Lines => Set<Line>();
+        public DbSet<Client> Clients => Set<Client>();
+
+        public DbSet<Material> Materials => Set<Material>();
+        public DbSet<TypeScrap> TypeScraps => Set<TypeScrap>();
+        public DbSet<Shift> Shifts => Set<Shift>();
+        public DbSet<Process> Processes => Set<Process>();
+        public DbSet<MachineCode> MachineCodes => Set<MachineCode>();
+        public DbSet<Scrap> Scraps => Set<Scrap>();
+        public DbSet<Defect> Defects => Set<Defect>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<UserRole>().ToTable("UserRoles");
-
-            modelBuilder.Entity<UserRole>()
-                .HasKey(ur => new { ur.UserId, ur.RoleId });
-
-            modelBuilder.Entity<UserRole>()
-                .HasOne(ur => ur.User)
-                .WithMany(u => u.UserRoles)
-                .HasForeignKey(ur => ur.UserId);
-
-            modelBuilder.Entity<UserRole>()
-                .HasOne(ur => ur.Role)
-                .WithMany(r => r.UserRoles)
-                .HasForeignKey(ur => ur.RoleId);
-
-            modelBuilder.Entity<Scrap>(entity =>
+            modelBuilder.Entity<UserRole>(entity =>
             {
-                entity.ToTable("Scrap");
+                entity.ToTable("UserRoles");
 
-                entity.Property(e => e.CreatedAt).HasDefaultValueSql("GETDATE()");
+                entity.HasKey(ur => new { ur.UserId, ur.RoleId });
 
-                entity.HasOne(d => d.Line)
-                    .WithMany(p => p.Scraps)
-                    .HasForeignKey(d => d.LineId)
-                    .OnDelete(DeleteBehavior.NoAction);
+                entity.HasOne(ur => ur.User)
+                    .WithMany(u => u.UserRoles)
+                    .HasForeignKey(ur => ur.UserId);
 
-                entity.HasOne(d => d.Defect)
-                    .WithMany()
-                    .HasForeignKey(d => d.DefectId)
-                    .OnDelete(DeleteBehavior.NoAction);
+                entity.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId);
             });
 
             modelBuilder.Entity<Role>().ToTable("Roles");
+            modelBuilder.Entity<User>().ToTable("Users");
+
             modelBuilder.Entity<Line>().ToTable("Lines");
             modelBuilder.Entity<Client>().ToTable("Clients");
-            modelBuilder.Entity<TypeScrap>().ToTable("TypeScrap");
-            modelBuilder.Entity<MachineCode>().ToTable("MachineCodes");
             modelBuilder.Entity<Material>().ToTable("Material");
+            modelBuilder.Entity<TypeScrap>().ToTable("TypeScrap");
             modelBuilder.Entity<Shift>().ToTable("Shifts");
             modelBuilder.Entity<Process>().ToTable("Process");
+            modelBuilder.Entity<MachineCode>().ToTable("MachineCodes");
             modelBuilder.Entity<Defect>().ToTable("Defects");
 
             modelBuilder.Entity<DefectRejection>(entity =>
             {
                 entity.ToTable("DefectsRejections");
-                entity.Property(e => e.DefectName).HasMaxLength(70);
+
+                entity.Property(e => e.DefectName)
+                    .HasMaxLength(70);
             });
 
             modelBuilder.Entity<Condition>(entity =>
             {
                 entity.ToTable("Conditions");
-                entity.Property(e => e.ConditionName).HasMaxLength(70);
+
+                entity.Property(e => e.ConditionName)
+                    .HasMaxLength(70);
 
                 entity.HasOne(c => c.DefectRejection)
                     .WithMany(d => d.Conditions)
@@ -108,7 +85,66 @@ namespace Infrastructure.Data
             modelBuilder.Entity<ContainmentAction>(entity =>
             {
                 entity.ToTable("ContainmentAction");
-                entity.Property(e => e.ContainmentActionName).HasMaxLength(30);
+
+                entity.Property(e => e.ContainmentActionName)
+                    .HasMaxLength(30);
+            });
+
+            modelBuilder.Entity<Rejection>(entity =>
+            {
+                entity.ToTable("Rejections");
+
+                entity.Property(r => r.CreatedAt)
+                    .HasDefaultValueSql("GETDATE()");
+
+                entity.HasOne<DefectRejection>(r => r.Defect)
+                    .WithMany()
+                    .HasForeignKey(r => r.DefectId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(r => r.Condition)
+                    .WithMany()
+                    .HasForeignKey(r => r.ConditionId)
+                    .OnDelete(DeleteBehavior.NoAction);
+
+                entity.HasOne(r => r.Line)
+                    .WithMany()
+                    .HasForeignKey(r => r.LineId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(r => r.Client)
+                    .WithMany()
+                    .HasForeignKey(r => r.ClientId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(r => r.ContainmentAction)
+                    .WithMany()
+                    .HasForeignKey(r => r.ContainmentActionId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(r => r.User)
+                    .WithMany()
+                    .HasForeignKey(r => r.UserId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+
+
+            modelBuilder.Entity<Scrap>(entity =>
+            {
+                entity.ToTable("Scrap");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasDefaultValueSql("GETDATE()");
+
+                entity.HasOne(d => d.Line)
+                    .WithMany(p => p.Scraps)
+                    .HasForeignKey(d => d.LineId)
+                    .OnDelete(DeleteBehavior.NoAction);
+
+                entity.HasOne(d => d.Defect)
+                    .WithMany()
+                    .HasForeignKey(d => d.DefectId)
+                    .OnDelete(DeleteBehavior.NoAction);
             });
         }
     }

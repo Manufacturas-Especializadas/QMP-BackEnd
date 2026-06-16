1e1b5e7f523ac4717c62012aa0bcd31ba85c2da6
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-16 14:55:43 -0600

    feat(IRepository): interfaces para el registro y lista de las auditorias FCDS

---
+++ Core/Interfaces/IAuditFcdsRepository.cs
@@ -0,0 +1,11 @@
+﻿using Core.DTOs;
+
+namespace Core.Interfaces
+{
+    public interface IAuditFcdsRepository
+    {
+        Task<bool> CreateAuditAsync(CreateAuditFcdsDto dto, int userId);
+
+        Task<IEnumerable<AuditFcdsListDto>> GetListAuditsAsync();
+    }
+}

b642b28f19b818e2e4ec32488835eb82d124fb51
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-16 11:32:11 -0600

    feat(repositories): repositorios de rechazos(incompletos)

---
+++ Core/Interfaces/IDefectRejectionRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+
+
+namespace Core.Interfaces
+{
+    public interface IDefectRejectionRepository
+    {
+        Task<DefectRejection> GetByIdAsync(int id);
+
+        Task<DefectRejection> CreateAsync(DefectRejection defect);
+
+        Task<DefectRejection> UpdateAsync(int id, DefectRejection defect);
+
+        Task<bool> DeleteAsync(int id);
+
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ Infrastructure/Repositories/DefectRejectionRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Interfaces;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories
+{
+    public class DefectRejectionRepository /*: IDefectRejectionRepository*/
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DefectRejectionRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}

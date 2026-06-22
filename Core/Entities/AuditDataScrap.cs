a50a812bda2cd3c0cab50e7cffe93ef9e39ceaaa
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-22 08:54:46 -0600

    feat(entities): entidades para auditorias de scrap

---
+++ Core/Entities/AuditDataScrap.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.Entities
+{
+    public class AuditDataScrap
+    {
+        public int Id { get; set; }
+
+        public DateTime AuditDate { get; set; }
+
+        public int UserId { get; set; }
+
+        public int ShiftId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual Shift Shift { get; set; }
+
+        public virtual ICollection<Line> Lines { get; set; } = new HashSet<Line>();
+
+        public virtual ICollection<AuditFindingScrap> Findings { get; set; } = new HashSet<AuditFindingScrap>();
+    }
+}
---
+++ Core/Entities/AuditFindingScrap.cs
@@ -0,0 +1,28 @@
+﻿namespace Core.Entities
+{
+    public class AuditFindingScrap
+    {
+        public int Id { get; set; }
+
+        public int AuditId { get; set; }
+
+        public int TypeScrapId { get; set; }
+
+        public decimal EstimatedWeight { get; set; }
+
+        public byte MaterialCorrectlyIdentified { get; set; }
+
+        public byte MaterialCorrectlySegregated { get; set; }
+
+        public string? UnreportedReason { get; set; }
+
+        public string? ImageEvidence { get; set; }
+
+        public string? SupervisorSignature { get; set; }
+
+        public virtual AuditDataScrap Audit { get; set; } = null!;
+
+        public virtual TypeScrap TypeScrap { get; set; } = null!;
+
+    }
+}

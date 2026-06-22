853bcc5aa4cf8c869901a24db67df629d8c1a96e
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-22 09:09:47 -0600

    feat(IRepository): Interfaces CRUD para auditorias de scrap

---
+++ Core/Interfaces/IAuditScrapRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Core.DTOs;
+
+namespace Core.Interfaces
+{
+    public interface IAuditScrapRepository
+    {
+        Task<AuditDataScrap?> GetByIdAsync(int id);
+
+        Task<IEnumerable<AuditDataScrapReadDto>> GetAllDetailedAsync();
+
+        Task<IEnumerable<AuditDataScrap>> GetByMonthAsync(int month, int year);
+
+        Task<AuditDataScrap> CreateAsync(AuditDataScrap entity);
+
+        Task<bool> DeleteAsync(int id);
+
+        Task<bool> SaveChangesAsync();
+    }
+}

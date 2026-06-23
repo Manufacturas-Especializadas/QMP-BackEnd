082d2a2f519cb1fd7acded38def1dda0909eb614
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-23 10:24:12 -0600

    feat(entities): entidades relacionada a auditoras ACDS

---
+++ Core/Entities/AuditEndPoint.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities
+{
+    public class AuditEndPoint
+    {
+        public int Id { get; set; }
+
+        public string ProcessName { get; set; }
+
+        public virtual ICollection<AuditFindingACD> Findings { get; set; } = new HashSet<AuditFindingACD>();
+    }
+}
---
+++ Core/Entities/AuditFindingACD.cs
@@ -0,0 +1,41 @@
+﻿namespace Core.Entities
+{
+    public class AuditFindingACD
+    {
+        public int Id { get; set; }
+
+        public int AuditId { get; set; }
+
+        public int StartPointId { get; set; }
+
+        public int EndPointId { get; set; }
+
+        public string PartNumber { get; set; } = null!;
+
+        public int NumberOfPieces { get; set; }
+
+        public string SampleSize { get; set; } = null!;
+
+        public int PackerPayroll { get; set; }
+
+        public bool? ContainerIdMatch { get; set; }
+
+        public byte FrontView { get; set; }
+
+        public byte SideView { get; set; }
+
+        public byte TopView { get; set; }
+
+        public byte IsometricView { get; set; }
+
+        public bool? CompleteProcess { get; set; }
+
+        public bool IsProductConforming { get; set; }
+
+        public virtual AuditDataACD Audit { get; set; } = null!;
+
+        public virtual AuditStartPoint StartPoint { get; set; } = null!;
+
+        public virtual AuditEndPoint EndPoint { get; set; } = null!;
+    }
+}

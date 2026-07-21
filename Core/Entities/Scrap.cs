3dc6aa2fea4e78b3e5d97190f83a343a4c761616
HectorCamachoCorona <camacho.corona.223@gmail.com>
2026-07-21 15:26:00 -0600

    intento3 excel

--- Core/Entities/Scrap.cs
+++ Core/Entities/Scrap.cs
@@ -9,51 +9,18 @@ namespace Core.Entities
     public class Scrap
     {
         public int Id { get; set; }
-
         public int PayRollNumber { get; set; }
-
-        public string? Alloy { get; set; }
-
-        public string? Diameter { get; set; }
-
-        public string? Wall { get; set; }
-
-        public decimal? Weight { get; set; }
-
-        public string RDM { get; set; } = string.Empty;
-
-        public bool IsVerified { get; set; } = false;
-
-        public decimal? VerifiedWeight { get; set; } 
-
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-        
         public int ShiftId { get; set; }
-
         public int? ProcessId { get; set; }
-
         public int LineId { get; set; }
-
-        public int MaterialId { get; set; }
-
-        public int? MachineCodeId {  get; set; }
-
-        public int TypeScrapId { get; set; }
-
-        public int? DefectId { get; set; }
+        public int? MachineCodeId { get; set; }
 
         public Shift Shift { get; set; } = null!;
-
         public Process? Process { get; set; }
-
         public Line Line { get; set; } = null!;
-
         public MachineCode MachineCode { get; set; } = null!;
 
-        public Material Material { get; set; } = null!;
-
-        public TypeScrap TypeScrap { get; set; } = null!;
-
-        public Defect Defect { get; set; } = null!;
+        public ICollection<ScrapDetail> ScrapDetails { get; set; } = new List<ScrapDetail>();
     }
 }
